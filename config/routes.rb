0488ed1cbb8304d3313d1f9ad7a48927350980c7
Prelang Builder <builder@prelang.com>
2014-07-07 16:39:16 +0000

    Set up member route for #vote action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :guitars
+  resources :guitars do
+  #->Prelang (voting/acts_as_votable)
+  member do
+    get "vote"
+  end
+end
+
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
